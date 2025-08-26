e95a1f35974a1fa33a085b1b83b383ffe8f90255
Rohit kumar rana <188756253+rohitkumarrana1998@users.noreply.github.com>
2025-08-26 09:41:24 +0000

    lab

---
+++ lab3.py
@@ -0,0 +1,4 @@
+num1 = float(intput("Enter first number"))
+num2 = float(intput("Enter second number"))
+result = num1 + num2
+print("sum:" , result)
---
+++ lab5:.py
@@ -0,0 +1,5 @@
+for i in range (1,11):
+    if i==2:
+        break
+    for j in range (2,11):
+        print("Decimal equivalent of ",i,"=",i/j)
